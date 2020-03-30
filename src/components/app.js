1f53148878f5269bd3a5c98e37f5cdfc595c1001
Fran Jurmanović <fjurma12@outlook.com>
2020-03-30 17:30:16 +0200

    React skeleton

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    callAPI() {
+        fetch("http://localhost:3000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+    
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    render() {
+    return(<h1>{this.state.apiResponse}</h1>);
+    }
+}
+
+export default App;
